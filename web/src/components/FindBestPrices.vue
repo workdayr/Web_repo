18fb366ed5fdd521cdbd81cfbfa1a6169f9c88ff
Killua <fridaolague@hotmail.com>
2025-02-20 14:11:43 -0600

    Cambio de color de boton

--- web/src/components/FindBestPrices.vue
+++ web/src/components/FindBestPrices.vue
@@ -3,7 +3,7 @@
     <h2 class="cta-container__welcomeText">
       Start finding the best prices for you
     </h2>
-    <h5 class="welcomeText__description">Click and find your best <br> deal</h5>
+    <h5 class="welcomeText__description">Finding sales has never<br>been this easy</h5>
     <button class="cta-button">Sign up</button>
   </section>
 </template>
@@ -15,8 +15,4 @@ export default {};
 <style scoped>
 @import "@/assets/styles/FindBestPrices.css";
 
-h2 {
-  font-size: 2rem;
-  font-weight: bold;
-}
 </style>
