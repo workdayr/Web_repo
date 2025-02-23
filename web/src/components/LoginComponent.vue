878330431155f8aea3d8b86a9357c39cc75f1c43
Alejandro Arellano <a.arellano.h337@gmail.com>
2025-02-22 20:21:26 -0600

    Log In

--- web/src/components/LoginComponent.vue
+++ web/src/components/LoginComponent.vue
@@ -1,22 +1,42 @@
 <template>
-    <button @click="$router.push('/')" class="backHome">BackHome</button>
-    <div id="login_background">
-        <div class="background_shape">
+    <div id="login__background">
+        <img class="background__shape1" src="@/assets/svg/Vector.svg" alt="Vector">
+        <img class="background__shape2" src="@/assets/svg/backgroundShape.svg" alt="backgroundSape">
+    </div>
+    <div class="login__container">
+        <h1 class="login__container--header"> Log In</h1>
+        <input class="login__input--username" type="text" placeholder="Username" aria-label="Username">
+        <input class="login__input--password" type="text" placeholder="Password" aria-label="Password">
+        <div id="login__inputs">
+            <label>
+                <input id="login__input--rememberMe" type="checkbox" name="Remember me">
+                <span class="input__rememberMe--label">Remember me</span>
+            </label>
+            <a class="login__link--forgotPw" @click="$router.push('/')">Forgot password?</a>
+        </div>
+        <button class="login__button">NEXT</button>
+        <div id="login__other-login-options">
+            <p>Other <span class="login__other-login-options--highlited-word">Login </span>options</p>
+            <div class="other-login-opitons__icons">
+                <img class="GoogleIcon" src="@/assets/svg/Googlelogo.svg" alt="Google">
+                <img class="FacebookIcon" src="@/assets/svg/Facebooklogo.svg" alt="Facebook">
+                <img class="AppleIcon" src="@/assets/svg/Applelogo.svg" alt="Apple">
+            </div>
+            <div id="create-account-container">
+                <img class="userIcon" src="@/assets/svg/UserIcon.svg" alt="UserIcon"><a class="create-account-container__header">Create account</a>
+
+            </div>
+
 
         </div>
-        
-
-
-
     </div>
 </template>
 <script>
 export default {
-    name:'LoginComponent',
-    
+    name: 'LoginComponent',
+
 }
 </script>
 <style scoped>
 @import "@/assets/styles/LoginComponent.css"
-    
 </style>
