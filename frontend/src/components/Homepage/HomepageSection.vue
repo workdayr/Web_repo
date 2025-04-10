53c9ee2a742922f257f7db5567a51bcc430b7129
J-R <josjacrosales@gmail.com>
2025-04-09 18:55:29 -0600

    feat: add store image to section, fix form error message

--- frontend/src/components/Homepage/HomepageSection.vue
+++ frontend/src/components/Homepage/HomepageSection.vue
@@ -1,18 +1,19 @@
 <script setup>
-import { defineProps} from 'vue';
+import { defineProps, ref} from 'vue';
 import HomepageProductRow from '@/components/Homepage/HomepageProductRow.vue';
 defineProps({
   title: String, // Section Title
   icon: String,
   products: Array // List of products
 });
+const activeIcon = ref(true);
 </script>
 
 <template>
   <section class="section-container">
     <div class="section-header">
       <h1 class="section-title">{{ title }}</h1>
-      <img v-if="icon" :src="icon" class="section-icon">
+      <img v-if="icon && activeIcon" :src="icon" class="section-icon" @error="activeIcon = false">
     </div>
     <HomepageProductRow :products="products"/>
   </section>
